cf254965b2879b268ccba9a440fcd1597affd65f
cblnam412 <23520979@gm.uit.edu.vn>
2024-11-24 03:37:31 +0700

    Add files via upload

---
+++ Snake/TaiKhoan.sql
@@ -0,0 +1,17 @@
+create QLTK
+go
+use QLTK
+go
+
+create table [User]
+(
+	Id UNIQUEIDENTIFIER primary Key default NEWID(),
+	Username nvarchar (50) unique not null,
+	[Password] nvarchar (100) not null,
+)
+go
+insert into [User] values (NEWID(), 'user01', '123')
+insert into [User] values (NEWID(), 'user02', '124')
+insert into [User] values (NEWID(), 'user03', '126')
+go
+select *from [User]
