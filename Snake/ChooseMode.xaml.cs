aa6b77fbfec5838fd51c8d01279358355b810f2d
Porcup13 <23521064@gm.uit.edu.vn>
2024-11-25 21:24:54 +0700

    ui choosemode
    choosemode

---
+++ Snake/ChooseMode.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Snake
+{
+    /// <summary>
+    /// Interaction logic for ChooseMode.xaml
+    /// </summary>
+    public partial class ChooseMode : Window
+    {
+        public ChooseMode()
+        {
+            InitializeComponent();
+        }
+
+        private void Backmode_click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Nextmode_click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Back_click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
