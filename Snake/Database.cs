4388dd7629a82afd110b5fd57212a80dd368e6e9
cblnam412 <23520979@gm.uit.edu.vn>
2024-11-29 19:54:22 +0700

    Add files via upload

--- Snake/Database.cs
+++ Snake/Database.cs
@@ -24,8 +24,11 @@ namespace Snake
 
                 using (SqlCommand command = new SqlCommand(query, connection))
                 {
+                    // Hash mật khẩu bằng bcrypt
+                    string hashedPassword = BCrypt.Net.BCrypt.HashPassword(password);
+
                     command.Parameters.AddWithValue("@Username", username);
-                    command.Parameters.AddWithValue("@Password", password);
+                    command.Parameters.AddWithValue("@Password", hashedPassword);
 
                     try
                     {
@@ -34,30 +37,32 @@ namespace Snake
                     }
                     catch (SqlException ex)
                     {
-                        if (ex.Number == 2627)
-                        {
-                            MessageBox.Show("User đã tồn tại!!");
-                        }
+                        MessageBox.Show("User đã tồn tại!!");
                         return false;
                     }
                 }
             }
         }
+
+
         // Hàm kiểm tra đăng nhập
         public static bool ValidateUser(string username, string password)
         {
             using (SqlConnection connection = new SqlConnection(ConnectionString))
             {
                 connection.Open();
-                string query = "SELECT COUNT(1) FROM [User] WHERE Username = @Username AND Password = @Password;";
+                string query = "SELECT Password FROM [User] WHERE Username = @Username;";
 
                 using (SqlCommand command = new SqlCommand(query, connection))
                 {
                     command.Parameters.AddWithValue("@Username", username);
-                    command.Parameters.AddWithValue("@Password", password);
 
-                    int count = (int)command.ExecuteScalar();
-                    return count > 0;
+                    // Lấy mật khẩu đã hash từ cơ sở dữ liệu
+                    string storedHash = command.ExecuteScalar()?.ToString();
+                    if (storedHash == null) return false;
+
+                    // So sánh mật khẩu đã nhập với mật khẩu đã hash
+                    return BCrypt.Net.BCrypt.Verify(password, storedHash);
                 }
             }
         }
