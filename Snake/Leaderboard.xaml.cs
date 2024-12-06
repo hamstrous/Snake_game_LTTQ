516905a6d23cff09f7a32b2ee7100e04c13bdadb
Porcup13 <23521064@gm.uit.edu.vn>
2024-12-06 13:21:53 +0700

    leaderboard
    leaderboard

---
+++ Snake/Leaderboard.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Snake
+{
+    /// <summary>
+    /// Interaction logic for Leaderboard.xaml
+    /// </summary>
+    public partial class Leaderboard : Window
+    {
+        public Leaderboard()
+        {
+            InitializeComponent();
+        }
+
+        private void Back_click (object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Click");
+        }
+
+        private void Next_click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Click");
+        }
+
+        private void Return_click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Click");
+        }
+        
+    }
+}
