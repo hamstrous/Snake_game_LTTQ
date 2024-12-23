50f0ea7e6d49dcab7b252838342a30131406b6a3
23521093 <156832544+hamstrous@users.noreply.github.com>
2024-12-24 02:35:59 +0700

    render

--- Snake/obj/Release/net8.0-windows/Snake.AssemblyInfo.cs
+++ Snake/obj/Release/net8.0-windows/Snake.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Snake")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf93ac913badfa35d492604594a3c645fc83876e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+faa590ad98d767b3a7983a93262946839b0d4d24")]
 [assembly: System.Reflection.AssemblyProductAttribute("Snake")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Snake")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
