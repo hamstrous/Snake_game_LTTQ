7c5e6431eb97b8a1da614442507fe90f0e454a98
23521093 <156832544+hamstrous@users.noreply.github.com>
2024-12-23 11:13:15 +0700

    lol

--- Snake/obj/Debug/net8.0-windows/Snake.AssemblyInfo.cs
+++ Snake/obj/Debug/net8.0-windows/Snake.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Snake")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07b31307ae7787e5342bc6bdfaafccf153eb8e31")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1a9eda3e7e35eb25430955007391518129bf290")]
 [assembly: System.Reflection.AssemblyProductAttribute("Snake")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Snake")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
