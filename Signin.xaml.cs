42cca3cea107db9faeba0d14b8e91a0f2dcf2bfb
cblnam412 <23520979@gm.uit.edu.vn>
2024-12-26 08:41:05 +0700

    Chuyển màn hình sau khi Sign in/up thành công

---
+++ Signin.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Snake
+{
+    /// <summary>
+    /// Interaction logic for SignIn.xaml
+    /// </summary>
+    public partial class SignIn : Window
+    {
+        public static string currentUserName { get; set; }
+        public SignIn()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnSignIn_Click (object sender, RoutedEventArgs e)
+        {
+            string username = txtusername.Text;
+            string password = txtpassword.Password;
+
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                txtErrorMessage.Text = "Please enter all the required information.";
+                txtErrorMessage.Visibility = Visibility.Visible;
+                return;
+            }
+
+            bool isValidUser = await Database.ValidateUserAsync(username, password);
+            if (isValidUser)
+            {
+                currentUserName = txtusername.Text;
+                txtErrorMessage.Text = "Login successful!!";
+                txtErrorMessage.Visibility = Visibility.Visible;
+                MainMenu mainMenu = new MainMenu();
+                mainMenu.Show();
+                this.Close();
+            }
+            else
+            {
+                txtErrorMessage.Text = "Incorrect username or password!!";
+                txtErrorMessage.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void btnSignUp_Click(object sender, RoutedEventArgs e)
+        {
+            SignUp signInWindow = new SignUp();
+            signInWindow.Show();
+            txtErrorMessage.Visibility = Visibility.Collapsed;
+            this.Close();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
